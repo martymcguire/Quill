dba333f8c1c7d98fdbb1b7ddca32903914c64ed8
Marty McGuire <schmartissimo@gmail.com>
2019-06-30 20:46:09 -0700

    oops, don't actually change Quill's name 😅

--- views/partials/header.php
+++ views/partials/header.php
@@ -1,4 +1,4 @@
 <div class="header">
   <a href="/dashboard"><img src="/images/quill-logo.png" width="42" height="42"></a>
-  <a href="/dashboard">Definitely not Quill</a>
+  <a href="/dashboard">Quill</a>
 </div>
